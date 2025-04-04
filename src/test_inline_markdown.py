8ee89403083d4b386694251622b72e41549e434c
Francisco Cruz <franciscohuertas25@gmail.com>
2025-04-04 09:06:13 -0500

    Update tests in inline_markdown

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -196,7 +196,8 @@ class TestInlineMarkdown(unittest.TestCase):
         self.assertListEqual(
             [
                 TextNode("This is text with an ", TextType.TEXT),
-                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode("image", TextType.IMAGE,
+                         "https://i.imgur.com/zjjcJKZ.png"),
             ],
             new_nodes,
         )
@@ -211,7 +212,8 @@ class TestInlineMarkdown(unittest.TestCase):
         self.assertListEqual(
             [
                 TextNode("This is text with an ", TextType.TEXT),
-                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode("image", TextType.IMAGE,
+                         "https://i.imgur.com/zjjcJKZ.png"),
                 TextNode(" and another ", TextType.TEXT),
                 TextNode(
                     "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
@@ -249,20 +251,22 @@ class TestInlineMarkdown(unittest.TestCase):
     def test_split_multiple_links(self):
         """Tests splitting nodes containing multiple links."""
         node = TextNode(
-                    "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
-                    TextType.TEXT,
-                )
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
+            TextType.TEXT,
+        )
         new_nodes = split_nodes_link([node])
         self.assertListEqual(
             [
                 TextNode("This is text with a ", TextType.TEXT),
                 TextNode("link", TextType.LINK, "https://boot.dev"),
                 TextNode(" and ", TextType.TEXT),
-                TextNode("another link", TextType.LINK, "https://blog.boot.dev"),
+                TextNode("another link", TextType.LINK,
+                         "https://blog.boot.dev"),
                 TextNode(" with text that follows", TextType.TEXT),
             ],
             new_nodes,
         )
+
 
 if __name__ == "__main__":
     unittest.main()
